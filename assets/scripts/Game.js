c10f271def2e2dde51064e8972f7e7ea9e6194fa
chenwenqi <wenqi@eastmoney.com>
2018-04-17 21:04:08 +0800

    添加脚本文件

---
+++ assets/scripts/Game.js
@@ -0,0 +1,51 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        mars: {
+            default: null,
+            type: cc.Node
+        },
+        earth: {
+            default: null,
+            type: cc.Node
+        },
+        blackHole: {
+            default: null,
+            type: cc.Node
+        },
+        scoreLabel: {
+            default: null,
+            type: cc.Label
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.director.getCollisionManager().enabled = true;
+        cc.director.getPhysicsManager().enabled = true;
+        // 去除重力
+        cc.director.getPhysicsManager().gravity = cc.v2();
+
+        this.score = 0;
+    },
+
+    start () {
+
+    },
+
+    update (dt) {
+
+    },
+});
