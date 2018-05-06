8b4fc004dadeca6cb779cc9cad969acbb70655e6
PC-KENNETH\FinLaas <finlaas@163.com>
2018-05-06 15:29:53 +0800

    suck

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -48,11 +48,31 @@ cc.Class({
             default: null,
             type: cc.Label
         },
+        failScoreLabel: {
+            default: null,
+            type: cc.Label
+        },
+        failUI: {
+            default: null,
+            type: cc.Node
+        },
         hitAudio: {
             default: null,
             url: cc.AudioClip,
         },
         holdAudio: {
+            default: null,
+            url: cc.AudioClip,
+        },
+        failAudio: {
+            default: null,
+            url: cc.AudioClip,
+        },
+        successNormalAudio: {
+            default: null,
+            url: cc.AudioClip,
+        },
+        successPerfectAudio: {
             default: null,
             url: cc.AudioClip,
         },
@@ -66,10 +86,10 @@ cc.Class({
         // 去除重力
         cc.director.getPhysicsManager().gravity = cc.v2();
 
-        //添加触摸监听
-        this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this);
-        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
-        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        // //添加触摸监听
+        // this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this);
+        // this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+        // this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
 
         //适配墙壁
         let width = this.node.width;
@@ -89,9 +109,11 @@ cc.Class({
     restart () {
         //clear score
         this.score = 0;
+        this.combo = 0;
+        this.scoreLabel.getComponent(cc.Label).string = '得分: ' + this.score;
+        this.failUI.active = false;
 
         this.mars.active = true;
-        //this.mars.linearVelocity = cc.v2(0,0);
 
         let width = this.node.width;
         let height = this.node.height;
@@ -99,6 +121,29 @@ cc.Class({
         this.mars.setPosition(Math.random() * width / 2 - width / 4, -height/3);
         this.earth.setPosition(Math.random() * width / 2 - width / 4, Math.random() * height / 4 - height / 8);
         this.blackHole.setPosition(Math.random() * width / 2 - width / 4, height / 3);
+
+        //添加触摸监听
+        this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this);
+        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+    },
+
+    continue () {
+        this.failUI.active = false;
+
+        this.mars.active = true;
+
+        let width = this.node.width;
+        let height = this.node.height;
+        
+        this.mars.setPosition(Math.random() * width / 2 - width / 4, -height/3);
+        this.earth.setPosition(Math.random() * width / 2 - width / 4, Math.random() * height / 4 - height / 8);
+        this.blackHole.setPosition(Math.random() * width / 2 - width / 4, height / 3);
+
+        //添加触摸监听
+        this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this);
+        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
     },
 
     onTouchBegan (event) {
@@ -106,6 +151,8 @@ cc.Class({
 
         this.touchTime = new Date();
         this.holdAudioID = cc.audioEngine.playEffect(this.holdAudio, false);
+
+        this.touchBagan = true;
     },
 
     onTouchEnd (event) {
@@ -130,6 +177,10 @@ cc.Class({
         rigidBody.applyLinearImpulse(vel,rigidBody.getWorldCenter(),true);
         //motion start
         this.marsBegan = true;
+        //restore size
+        this.touchBagan = false;
+        this.mars.width = 80;
+        this.mars.height = 80;
     },
 
     _setBound (node,x, y, width, height) {
@@ -143,15 +194,63 @@ cc.Class({
 
     },
 
+    fail () {
+        this.failScoreLabel.getComponent(cc.Label).string = this.score;
+
+        this.failUI.active = true;
+        
+        cc.audioEngine.playEffect(this.failAudio, false);
+        //添加触摸监听
+        this.node.off(cc.Node.EventType.TOUCH_START, this.onTouchBegan, this);
+        this.node.off(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+        this.node.off(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+    },
+
+    success (gap) {
+        if (gap < (this.blackHole.width / 2 - this.earth.width / 2)) {
+            //perfect
+            this.combo++;
+            this.score += this.combo * 2;
+            
+            cc.audioEngine.playEffect(this.successPerfectAudio, false);
+        } else {
+            this.combo = 0;
+            this.score++;
+            
+            cc.audioEngine.playEffect(this.successNormalAudio, false);
+        }
+
+        this.scoreLabel.getComponent(cc.Label).string = '得分: ' + this.score;
+        //suck 吸入
+        var finished = cc.callFunc(function () {
+            this.earth.scale = 1;
+            this.earth.getComponent(cc.RigidBody).angularVelocity = 0;
+            this.continue();
+        }, this);
+        var spawn = cc.sequence(cc.spawn(cc.moveTo(0.2,this.blackHole.position), cc.scaleTo(0.2, 0)), finished);
+        this.earth.getComponent(cc.RigidBody).angularVelocity = 2000;
+        this.earth.runAction(spawn);
+    },
+
     update (dt) {
         this.updateBg();
+
+        if (this.touchBagan) {
+            if (this.mars.width > 60) {
+                this.mars.width -= 0.3;
+                this.mars.height -= 0.3;
+            }
+            
+            return;
+        }
 
         if (this.marsBegan && !this.earth.onContact) {
             let marsVel = this.mars.getComponent(cc.RigidBody).linearVelocity;
             if (Math.abs(marsVel.x) <= 0.5 && Math.abs(marsVel.y) <= 0.5) {
                 //运动停止
                 this.marsBegan = false;
-                this.restart();
+                this.fail();
+                // this.restart();
                 return;
             }
         }
@@ -171,7 +270,13 @@ cc.Class({
                 this.earth.onContact = false;
                 cc.log('stop');
                 //do sth ..
-                this.restart();
+                var distance = this.earth.position.sub(this.blackHole.position);
+                var gap = Math.sqrt(distance.x * distance.x + distance.y * distance.y);
+                if (gap <= (this.earth.width / 2 + this.blackHole.width / 2)) {
+                    this.success(gap);
+                } else {
+                    this.fail();
+                }
             }
         }
     },
